b686d72aac2b0cba8757ce1d08b486d3728c8593
LuciaKr <56343765+LuciaKr@users.noreply.github.com>
2020-01-08 10:59:27 +0100

    Add files via upload

---
+++ keleidoskop.py
@@ -0,0 +1,20 @@
+import tkinter
+import random
+
+X_MAX, Y_MAX = 800,600
+
+canvas=tkinter.Canvas(width=X_MAX, height=Y_MAX, bg='white')
+canvas.pack()
+    
+def pohyb(event):
+    x,y = event.x, event.y
+    r=10
+    canvas.create_oval(x-r,y-r,x+r,y+r, fill='lavender')
+    a= X_MAX - x
+    canvas.create_oval(a-r,y-r,a+r,y+r, fill='sky blue')
+    b= Y_MAX - y
+    canvas.create_oval(x-r,b-r,x+r,b+r, fill='light yellow')
+    canvas.create_oval(a-r,b-r,a+r,b+r, fill='pink')
+    
+
+canvas.bind('<Motion>', pohyb)
