2de5c7e5039baa74ba58fd6043b9bc60d896055e
LuciaKr <56343765+LuciaKr@users.noreply.github.com>
2020-01-08 15:08:55 +0100

    Add files via upload

---
+++ KreslenieCiarky.py
@@ -0,0 +1,21 @@
+import tkinter
+from random import randrange, randint
+
+X_MAX,Y_MAX=800,600
+xx,yy=0,0
+
+canvas=tkinter.Canvas(width=X_MAX,height=Y_MAX,bg="white")
+canvas.pack()
+
+def kliknutie(event):
+    global xx,yy
+    canvas.bind("<B1-Motion>",ciarka)
+    xx,yy=event.x,event.y
+
+def ciarka(event):
+    global xx,yy
+    x,y=event.x,event.y
+    canvas.create_line(xx,yy,x,y,width=4, fill='sky blue')
+    xx,yy=x,y
+
+canvas.bind("<Button-1>", kliknutie)
